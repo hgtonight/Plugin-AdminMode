fafe9f2b80bee132e33da0dd6a7b81c1a6db5af4
Zachary Doll <hgtonight@gmail.com>
2014-07-21 13:22:08 -0500

    Add Russian translation, thanks Almaz!

---
+++ locale/ru-RU/definitions.php
@@ -0,0 +1,4 @@
+<?php if (!defined('APPLICATION')) exit();
+/* Copyright 2014 Zachary Doll */
+$Definition['Plugins.AdminMode.DisableLink'] = '<span class="InformSprite Lightbulb">Выключить режим тех. обслуживания</span>';
+$Definition['Plugins.AdminMode.EntryLink'] = '<span class="InformSprite Key">Авторизация</span>';
