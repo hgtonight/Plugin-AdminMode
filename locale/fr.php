1fe53289db48de431134d03b1dbd01501b31bf83
Zachary Doll <hgtonight@gmail.com>
2017-08-25 14:20:30 -0500

    Add French translation
    Added from https://open.vanillaforums.com/discussion/comment/249023/#Comment_249023:
    
    > @GGLinnk said:
    > PS 2 : I read and i'm agree with the GPLv2.
    > PS 3 : I'm agree in the fact that you use my translation into your plugin.

---
+++ locale/fr.php
@@ -0,0 +1,4 @@
+<?php if (!defined('APPLICATION')) exit();
+/* Copyright 2017 GGLinnk (ggsuper@orange.fr) */
+$Definition['Plugins.AdminMode.DisableLink'] = '<span class="InformSprite Lightbulb"></span> Quitter le mode maintenance';
+$Definition['Plugins.AdminMode.EntryLink'] = '<span class="InformSprite Key"></span> Se connecter';
